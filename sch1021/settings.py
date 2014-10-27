2eda30ed4eb22b5e3f657f36b073de43135caab3
Administrator <zdvitas@gmail.com>
2014-10-28 03:18:48 +0400

    Final

--- sch1021/settings.py
+++ sch1021/settings.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from os.path import join
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -58,7 +59,7 @@ DATABASES = {
 }
 
 TEMPLATE_DIRS = (
-    BASE_DIR + '/templates/'
+    join(BASE_DIR,  'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
